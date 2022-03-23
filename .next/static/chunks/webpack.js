e06221cdeac8396be26d9895724e7c66e3521281
j3su77 <kingj3su@gmail.com>
2022-03-23 12:07:28 -0500

    delete index.ts empty in hook/

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ccb86072386ed18"; }
+/******/ 		__webpack_require__.h = function() { return "9b6724888920bf3d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
